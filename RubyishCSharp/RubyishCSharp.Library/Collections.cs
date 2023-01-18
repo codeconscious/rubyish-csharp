f52228e3f69e53253b74a0fc29202feae6fbf334
CodeConscious <hello@codeconscio.us>
2023-01-18 23:09:12 +0900

    Add Reject method

--- RubyishCSharp/RubyishCSharp.Library/Collections.cs
+++ RubyishCSharp/RubyishCSharp.Library/Collections.cs
@@ -1,6 +1,21 @@
-﻿namespace RubyishCSharp.Library;
-public class Class1
+﻿using System.Collections.Generic;
+
+namespace RubyishCSharp.Library;
+
+public static class Collections
 {
-
+    /// <summary>
+    /// Returns a new collection from which items matching a given predicate
+    /// have been removed.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="collection"></param>
+    /// <param name="predicate"></param>
+    /// <returns>A new filtered collection.</returns>
+    public static IEnumerable<T> Reject<T>(
+        this IEnumerable<T> collection,
+        Func<T, bool> predicate)
+    {
+        return collection.Where(x => !predicate(x));
+    }
 }
-
