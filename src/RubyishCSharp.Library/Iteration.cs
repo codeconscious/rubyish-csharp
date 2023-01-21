fd7a63e437fe07ac996a77eb3ba5009d03cc012d
CodeConscious <hello@codeconscio.us>
2023-01-21 12:00:39 +0900

    Add Times()

---
+++ src/RubyishCSharp.Library/Iteration.cs
@@ -0,0 +1,332 @@
+﻿using System.Collections.Generic;
+
+namespace RubyishCSharp.Library;
+
+public static class Iterations
+{
+    /// <summary>
+    /// Run an action a specified number of times.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    /// <remarks>Example usage: 10.Times(</remarks>
+    public static void Times(this nint count, Action action)
+    {
+        if (count < 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be greater than or equal to 1.");
+        }
+
+        for (nint i = 0; i < count; i++)
+        {
+            action();
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times, passing the current iteration
+    /// number to it each time.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    public static void Times(this nint count, Action<nint> action)
+    {
+        if (count < 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be positive.");
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            action(i);
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    /// <remarks>Example usage: 10.Times(</remarks>
+    public static void Times(this nuint count, Action action)
+    {
+        if (count == 0)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be greater than or equal to 1.");
+        }
+
+        for (nuint i = 0; i < count; i++)
+        {
+            action();
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times, passing the current iteration
+    /// number to it each time.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    public static void Times(this nuint count, Action<nuint> action)
+    {
+        if (count < 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be positive.");
+        }
+
+        for (nuint i = 0; i < count; i++)
+        {
+            action(i);
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    /// <remarks>Example usage: 10.Times(</remarks>
+    public static void Times(this int count, Action action)
+    {
+        if (count < 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be greater than or equal to 1.");
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            action();
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times, passing the current iteration
+    /// number to it each time.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    public static void Times(this int count, Action<int> action)
+    {
+        if (count < 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be positive.");
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            action(i);
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    /// <remarks>Example usage: 10.Times(</remarks>
+    /// /// <summary>
+    /// Run an action a specified number of times.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    /// <remarks>Example usage: 10.Times(</remarks>
+    public static void Times(this uint count, Action action)
+    {
+        if (count == 0)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be greater than or equal to 1.");
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            action();
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times, passing the current iteration
+    /// number to it each time.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    public static void Times(this uint count, Action<uint> action)
+    {
+        if (count < 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be positive.");
+        }
+
+        for (uint i = 0; i < count; i++)
+        {
+            action(i);
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    /// <remarks>Example usage: 10.Times(</remarks>
+    public static void Times(this short count, Action action)
+    {
+        if (count < 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be greater than or equal to 1.");
+        }
+
+        for (short i = 0; i < count; i++)
+        {
+            action();
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times, passing the current iteration
+    /// number to it each time.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    public static void Times(this short count, Action<short> action)
+    {
+        if (count < 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be positive.");
+        }
+
+        for (short i = 0; i < count; i++)
+        {
+            action(i);
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    /// <remarks>Example usage: 10.Times(</remarks>
+    public static void Times(this ushort count, Action action)
+    {
+        if (count == 0)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be greater than or equal to 1.");
+        }
+
+        for (ushort i = 0; i < count; i++)
+        {
+            action();
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times, passing the current iteration
+    /// number to it each time.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    public static void Times(this ushort count, Action<int> action)
+    {
+        if (count < 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be positive.");
+        }
+
+        for (ushort i = 0; i < count; i++)
+        {
+            action(i);
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    /// <remarks>Example usage: 10.Times(</remarks>
+    public static void Times(this byte count, Action action)
+    {
+        if (count == 0)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be greater than or equal to 1.");
+        }
+
+        for (byte i = 0; i < count; i++)
+        {
+            action();
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times, passing the current iteration
+    /// number to it each time.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    public static void Times(this byte count, Action<int> action)
+    {
+        if (count < 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be positive.");
+        }
+
+        for (byte i = 0; i < count; i++)
+        {
+            action(i);
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    /// <remarks>Example usage: 10.Times(</remarks>
+    public static void Times(this sbyte count, Action action)
+    {
+        if (count < 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be greater than or equal to 1.");
+        }
+
+        for (sbyte i = 0; i < count; i++)
+        {
+            action();
+        }
+    }
+
+    /// <summary>
+    /// Run an action a specified number of times, passing the current iteration
+    /// number to it each time.
+    /// </summary>
+    /// <param name="count">The number of times to run the action.</param>
+    /// <param name="action">The action to be executed.</param>
+    public static void Times(this sbyte count, Action<int> action)
+    {
+        if (count < 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(count), "The count must be positive.");
+        }
+
+        for (sbyte i = 0; i < count; i++)
+        {
+            action(i);
+        }
+    }
+}
---
+++ src/RubyishCSharp.Tests/IterationTest.cs
@@ -0,0 +1,343 @@
+﻿using RubyishCSharp.Library;
+
+namespace RubyishCSharp.Tests;
+
+public static class IterationTest
+{
+    public static class TimesTests
+    {
+        public class Nint
+        {
+            [Fact]
+            public void Times_NormalExecution_RunsExpectedNumberOfTimes()
+            {
+                const nint iterationCount = 5000;
+                nint actualCount = 0;
+                iterationCount.Times(() => actualCount++);
+                Assert.Equal(iterationCount, actualCount);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIteration_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 5000;
+                var expectedItems = Enumerable.Range(0, iterationCount)
+                                              .Select(i => (nint) i);
+                var actualItems = new List<nint>();
+                iterationCount.Times(i => actualItems.Add(i));
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIterationInString_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var stringGenerator = new Func<nint, string>(i => $"Iteration #{i}");
+                var expectedItems = Enumerable.Range(0, iterationCount)
+                                              .Select(i => (nint) i)
+                                              .Select(stringGenerator);
+
+                var actualItems = new List<string>();
+                iterationCount.Times(i => actualItems.Add(stringGenerator(i)));
+
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_ZeroIterationCount_ThrowException()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => ((nint) 0).Times(() => Console.WriteLine()));
+            }
+
+            [Fact]
+            public void Times_NegativeIterationCount_ThrowException()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => ((nint) (-1)).Times(() => Console.WriteLine()));
+            }
+        }
+
+        public class Nuint
+        {
+            [Fact]
+            public void Times_NormalExecution_RunsExpectedNumberOfTimes()
+            {
+                const nuint iterationCount = 5000;
+                nuint actualCount = 0;
+                iterationCount.Times(() => actualCount++);
+                Assert.Equal(iterationCount, actualCount);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIteration_RunsExpectedNumberOfTimes()
+            {
+                const nuint iterationCount = 5000;
+                var expectedItems = Enumerable.Range(0, (int) iterationCount)
+                                              .Select(i => (nuint) i);
+                var actualItems = new List<nuint>();
+                iterationCount.Times(actualItems.Add);
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIterationInString_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var stringGenerator = new Func<nint, string>(i => $"Iteration #{i}");
+                var expectedItems = Enumerable.Range(0, iterationCount)
+                                              .Select(i => (nint) i)
+                                              .Select(stringGenerator);
+
+                var actualItems = new List<string>();
+                iterationCount.Times(i => actualItems.Add(stringGenerator(i)));
+
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_ZeroIterationCount_ThrowException()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => ((nuint) 0).Times(() => Console.WriteLine()));
+            }
+        }
+
+        public class Int
+        {
+            [Fact]
+            public void Times_NormalExecution_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var actualCount = 0;
+                iterationCount.Times(() => actualCount++);
+                Assert.Equal(iterationCount, actualCount);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIteration_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var expectedItems = Enumerable.Range(0, iterationCount);
+                var actualItems = new List<int>();
+                iterationCount.Times(actualItems.Add);
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIterationInString_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var stringGenerator = new Func<int, string>(i => $"Iteration #{i}");
+                var expectedItems = Enumerable.Range(0, iterationCount)
+                                              .Select(stringGenerator);
+
+                var actualItems = new List<string>();
+                iterationCount.Times(i => actualItems.Add(stringGenerator(i)));
+
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_ZeroIterationCount_ThrowException()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => 0.Times(() => Console.WriteLine()));
+            }
+
+            [Fact]
+            public void Times_NegativeIterationCount_ThrowException()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => (-1).Times(() => Console.WriteLine()));
+            }
+        }
+
+        public class Short
+        {
+            [Fact]
+            public void Times_NormalExecution_RunsExpectedNumberOfTimes()
+            {
+                const short iterationCount = 500;
+                var actualCount = 0;
+                iterationCount.Times(() => actualCount++);
+                Assert.Equal(iterationCount, actualCount);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIteration_RunsExpectedNumberOfTimes()
+            {
+                const short iterationCount = 500;
+                var expectedItems = Enumerable.Range(0, iterationCount).Select(i => (short) i);
+                var actualItems = new List<short>();
+                iterationCount.Times(actualItems.Add);
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIterationInString_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var stringGenerator = new Func<int, string>(i => $"Iteration #{i}");
+                var expectedItems = Enumerable.Range(0, iterationCount)
+                                              .Select(stringGenerator);
+
+                var actualItems = new List<string>();
+                iterationCount.Times(i => actualItems.Add(stringGenerator(i)));
+
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_ZeroIterationCount_ThrowException()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => ((short) 0).Times(() => Console.WriteLine()));
+            }
+
+            [Fact]
+            public void Times_NegativeIterationCount_ThrowException()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => ((short) -1).Times(() => Console.WriteLine()));
+            }
+        }
+
+        public class Ushort
+        {
+            [Fact]
+            public void Times_NormalExecution_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var actualCount = 0;
+                iterationCount.Times(() => actualCount++);
+                Assert.Equal(iterationCount, actualCount);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIteration_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var expectedItems = Enumerable.Range(0, iterationCount);
+                var actualItems = new List<int>();
+                iterationCount.Times(actualItems.Add);
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIterationInString_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var stringGenerator = new Func<int, string>(i => $"Iteration #{i}");
+                var expectedItems = Enumerable.Range(0, iterationCount)
+                                              .Select(stringGenerator);
+
+                var actualItems = new List<string>();
+                iterationCount.Times(i => actualItems.Add(stringGenerator(i)));
+
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_ZeroIterationCount_ThrowException()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => ((ushort) 0).Times(() => Console.WriteLine()));
+            }
+        }
+
+        public class Byte
+        {
+            [Fact]
+            public void Times_NormalExecution_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var actualCount = 0;
+                iterationCount.Times(() => actualCount++);
+                Assert.Equal(iterationCount, actualCount);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIteration_RunsExpectedNumberOfTimes()
+            {
+                const byte iterationCount = 50;
+                var expectedItems = Enumerable.Range(0, iterationCount)
+                                              .Select(i => (byte) i);
+                var actualItems = new List<byte>();
+                iterationCount.Times(i => actualItems.Add((byte) i));
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIterationInString_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var stringGenerator = new Func<int, string>(i => $"Iteration #{i}");
+                var expectedItems = Enumerable.Range(0, iterationCount)
+                                              .Select(stringGenerator);
+
+                var actualItems = new List<string>();
+                iterationCount.Times(i => actualItems.Add(stringGenerator(i)));
+
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_ZeroIterationCount_ThrowException()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => ((byte) 0).Times(() => Console.WriteLine()));
+            }
+        }
+
+        public class Sbyte
+        {
+            [Fact]
+            public void Times_NormalExecution_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var actualCount = 0;
+                iterationCount.Times(() => actualCount++);
+                Assert.Equal(iterationCount, actualCount);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIteration_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var expectedItems = Enumerable.Range(0, iterationCount);
+                var actualItems = new List<int>();
+                iterationCount.Times(actualItems.Add);
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_PassingCurrentIterationInString_RunsExpectedNumberOfTimes()
+            {
+                const int iterationCount = 500;
+                var stringGenerator = new Func<int, string>(i => $"Iteration #{i}");
+                var expectedItems = Enumerable.Range(0, iterationCount)
+                                              .Select(stringGenerator);
+
+                var actualItems = new List<string>();
+                iterationCount.Times(i => actualItems.Add(stringGenerator(i)));
+
+                Assert.Equal(expectedItems, actualItems);
+            }
+
+            [Fact]
+            public void Times_ZeroIterationCount_ThrowException()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => ((sbyte) 0).Times(() => Console.WriteLine()));
+            }
+
+            [Fact]
+            public void Times_NegativeIterationCount_ThrowException()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(
+                    () => ((sbyte) -1).Times(() => Console.WriteLine()));
+            }
+        }
+    }
+}
